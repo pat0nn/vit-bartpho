ecef3def711d41cd794a86c45e8f65ce5ebb4dee
pat0nn <phambatrong.pbt@gmail.com>
2025-03-30 00:50:12 +0700

    done

--- config.py
+++ config.py
@@ -4,12 +4,20 @@
 IMAGE_ENCODER_MODEL = "google/vit-large-patch16-224-in21k"
 TEXT_DECODER_MODEL = "vinai/bartpho-word"
 
+# # Data paths
+# TRAIN_DATA_PATH = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/train_data.json'
+# TEST_DATA_PATH = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/test_data.json'
+# TRAIN_IMAGES_DIR = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/train-images'
+# TEST_IMAGES_DIR = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/public-test-images'
+# GROUNDTRUTH_FILE = '/run/media/trong/New Volume/Algo/ViT-BARTpho/data/groundtruth_captions.json'
+
 # Data paths
-TRAIN_DATA_PATH = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/train_data.json'
-TEST_DATA_PATH = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/test_data.json'
-TRAIN_IMAGES_DIR = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/train-images'
-TEST_IMAGES_DIR = '/run/media/trong/New Volume/Algo/KTVIC/processed_ktvic/public-test-images'
-GROUNDTRUTH_FILE = '/run/media/trong/New Volume/Algo/ViT-BARTpho/data/groundtruth_captions.json'
+TRAIN_DATA_PATH = '/kaggle/input/ktvic-bartpho/data/train_data.json'
+TEST_DATA_PATH = '/kaggle/input/ktvic-bartpho/data/test_data.json'
+TRAIN_IMAGES_DIR = '/kaggle/input/ktvic-bartpho/data/train-images'
+TEST_IMAGES_DIR = '/kaggle/input/ktvic-bartpho/data/public-test-images'
+
+GROUNDTRUTH_FILE = '/kaggle/working/ViT-BARTpho/data/groundtruth_captions.json'
 
 # Training parameters
 MAX_TARGET_LENGTH = 64
@@ -36,4 +44,4 @@ WANDB_PROJECT = "ViT-BARTpho"
 WANDB_NAME = "experiment"
 
 # Device
-DEVICE = 'cpu'  # 'cuda' or 'cpu'
+DEVICE = 'cuda'  # 'cuda' or 'cpu'
