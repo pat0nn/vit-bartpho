1307a134c8023abc0d857e10539b5161f0ecfd66
pat0nn <phambatrong.pbt@gmail.com>
2025-04-19 09:42:41 +0700

    done

--- config.py
+++ config.py
@@ -4,20 +4,20 @@
 IMAGE_ENCODER_MODEL = "google/vit-large-patch16-224-in21k"
 TEXT_DECODER_MODEL = "vinai/bartpho-word"
 
-# Data paths
-TRAIN_DATA_PATH = '/run/media/trong/New Volume/Algo/UIT-ViIC/annotations/uitviic_captions_train2017_v2.json'
-TEST_DATA_PATH = '/run/media/trong/New Volume/Algo/UIT-ViIC/annotations/uitviic_captions_test2017_v2.json'
-TRAIN_IMAGES_DIR = '/run/media/trong/New Volume/Algo/UIT-ViIC/uitviic_train2017'
-TEST_IMAGES_DIR = '/run/media/trong/New Volume/Algo/UIT-ViIC/uitviic_test2017'
-GROUNDTRUTH_FILE = '/run/media/trong/New Volume/Algo/vit-bartpho/data/groundtruth_captions_test2017.json'
-
 # # Data paths
-# TRAIN_DATA_PATH = '/kaggle/input/ktvic-bartpho/data/train_data.json'
-# TEST_DATA_PATH = '/kaggle/input/ktvic-bartpho/data/test_data.json'
-# TRAIN_IMAGES_DIR = '/kaggle/input/ktvic-bartpho/data/train-images'
-# TEST_IMAGES_DIR = '/kaggle/input/ktvic-bartpho/data/public-test-images'
+# TRAIN_DATA_PATH = '/run/media/trong/New Volume/Algo/UIT-ViIC/annotations/uitviic_captions_train2017_v2.json'
+# TEST_DATA_PATH = '/run/media/trong/New Volume/Algo/UIT-ViIC/annotations/uitviic_captions_test2017_v2.json'
+# TRAIN_IMAGES_DIR = '/run/media/trong/New Volume/Algo/UIT-ViIC/uitviic_train2017'
+# TEST_IMAGES_DIR = '/run/media/trong/New Volume/Algo/UIT-ViIC/uitviic_test2017'
+# GROUNDTRUTH_FILE = '/run/media/trong/New Volume/Algo/vit-bartpho/data/groundtruth_captions_test2017.json'
 
-# GROUNDTRUTH_FILE = '/kaggle/working/vit-bartpho/data/groundtruth_captions.json'
+# Data paths
+TRAIN_DATA_PATH = '/kaggle/input/uit-viic/annotations/uitviic_captions_train2017_v2.json'
+TEST_DATA_PATH = '/kaggle/input/uit-viic/annotations/uitviic_captions_test2017_v2.json'
+TRAIN_IMAGES_DIR = '/kaggle/input/uit-viic/uitviic_train2017'
+TEST_IMAGES_DIR = '/kaggle/input/uit-viic/uitviic_test2017'
+
+GROUNDTRUTH_FILE = '/kaggle/working/vit-bartpho/data/groundtruth_captions_test2017.json'
 
 # Training parameters
 MAX_TARGET_LENGTH = 64
@@ -28,7 +28,7 @@ WEIGHT_DECAY = 1e-6
 USE_FP16 = True
 
 # Quick testing/subset parameters
-USE_SUBSET = True  # Set to True for quick testing with subset of data
+USE_SUBSET = False  # Set to True for quick testing with subset of data
 TRAIN_SUBSET_SIZE =7  # Number of samples to use from training set
 TEST_SUBSET_SIZE = 7    # Number of samples to use from test set
 
@@ -40,8 +40,8 @@ NUM_BEAMS = 3
 DATASET_SAVE_PATH = './dataset/image_caption_dataset'
 OUTPUT_DIR = './output'
 LOGS_DIR = './logs'
-WANDB_PROJECT = "ViT-BARTpho_3-30"
+WANDB_PROJECT = "ViT-BARTpho_UIT-ViIC_4-19"
 WANDB_NAME = "experiment"
 
 # Device
-DEVICE = 'cpu'  # 'cuda' or 'cpu'
+DEVICE = 'cuda'  # 'cuda' or 'cpu'
